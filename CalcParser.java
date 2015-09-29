abfefa8267b632f488f1bb1717287a8728446228
Andrew Petriccione <apetricc@unca.edu>
2015-09-29 18:53:24 -0400

    added parser file need to make the methods to do the things that need to happen in the pseudocode, and making the methods should inform the making of the variables etc.

--- CalcParser.java
+++ CalcParser.java
@@ -12,6 +12,17 @@
  add op −→ + | -
  mult op −→ * | /
  Figure 2.15 LL(1) grammar for a simple calculator language.
+
+ Write a class called CalcParser. CalcParser should have one public method (aside from its constructor)
+ called parse() that starts a recursive descent parsing of a program written in the language of fig. 2.15
+ from the textbook. (See programming assignment #1 for the grammar.) The constructor should create an instance of
+ the CalcScanner class and pass it the input source file.
  */
 public class CalcParser {
+
+    private int myVar;
+    public CalcParser() {
+        int myVar;
+        myVar = this.myVar;
+    }
 }
