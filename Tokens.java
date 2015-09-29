f27eca37b5dc990873d3adc5d6ff3dc5aa9f2cbf
Andrew Petriccione <apetricc@unca.edu>
2015-09-29 18:53:57 -0400

    added parser file need to make the methods to do the things that need to happen in the pseudocode, and making the methods should inform the making of the variables etc.

---
+++ Tokens.java
@@ -0,0 +1,16 @@
+/**
+ * Created by petriccione on 9/14/15.
+ */
+public interface Tokens {
+
+    final int NUMBER = 500;
+    final int ERROR = -1;
+    final static int ASSIGN = 5555;
+    final int READ = 7777;
+    final int WRITE = 8888;
+    final int ID = 333;
+    final int DOUBLE_DOLLAR = 444;
+    final int EOF = 600;
+    final int COMMENT = 222;
+
+}
