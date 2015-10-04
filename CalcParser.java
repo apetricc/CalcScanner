7518490a27c2ca4e899a8bf637b51f6bfad14911
Andrew Petriccione <apetricc@unca.edu>
2015-10-04 18:54:00 -0400

    added pseudocode from moodle!!  converted it for the most part. gee, maybe it'll just work?!

--- CalcParser.java
+++ CalcParser.java
@@ -18,6 +18,85 @@
  from the textbook. (See programming assignment #1 for the grammar.) The constructor should create an instance of
  the CalcScanner class and pass it the input source file.
  */
+
+/** "OFFICIAL" PSEUDO-CODE:
+ *   parse() {
+ *       lookahead = nextToken();
+ *       program();
+ *       System.out.println("Parse success!");
+ *   }
+ *
+ *   program() {
+ *       if (lookahead == VAR) {
+ *           variable_list();
+ *       }
+ *       stmt_list()
+ *       match(EOP)
+ *   }
+ *   variable_list() {
+ *       match(VAR);
+ *       while(lookahead == ID) {
+ *           match(ID);
+ *       }
+ *       match(ENDVAR);
+ *   }
+ *   stmt_list() {
+ *       while(lookahead == ID || lookahead == READ || lookahead == WRITE)
+ *         stmt()
+ *   }
+ *   stmt() {
+ *       if (lookahead == ID) {
+ *           match(ID);
+ *           match(ASSIGN);
+ *           expr()
+ *       }
+ *       else if (lookahead == READ) {
+ *           match(READ);
+ *           match(ID);
+ *       }
+ *       else if (lookahead == WRITE) {
+ *           match(WRITE);
+ *           expr();
+ *       }
+ *       else error();
+ *   }
+ *   expr() {
+ *       term();
+ *       while (lookahead == ADDOP) {
+ *           match(ADDOP);
+ *           term();
+ *       }
+ *   }
+ *   term() {
+ *       factor();
+ *       while(lookahead == MULOP) {
+ *           match(MULOP);
+ *           factor();
+ *       }
+ *   }
+ *   factor() {
+ *       if (lookahead == LPAREN) {
+ *           match(LPAREN);
+ *           expr();
+ *           match(RPAREN);
+ *       }
+ *       else if (lookahead == ID) {
+ *           match(ID);
+ *       }
+ *       else if (lookahead == NUM) {
+ *           match(NUM);
+ *       }
+ *       else error();
+ *   }
+ *   match (token) {
+ *       if (lookahead == token) {
+ *           lookahead = nextToken();
+ *       }
+ *       else error();
+ *   }
+ *
+ *
+ */
 public class CalcParser {
 
     private int myVar;
