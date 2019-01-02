675810245e8fe85e3c51f68c551d7da92b1679c5
Andrew Petriccione <andrew.petriccione@metlife.com>
2019-01-02 14:04:09 -0500

    about ready to give up on this

--- MergeTestClass.java
+++ MergeTestClass.java
@@ -1,6 +1,8 @@
 
 public class MergeTestClass {
-		
+		//maybe this will cause a problem?
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Changed this whole line--hopefully that makes a conflict.");
 		System.out.println("Changed this whole line--hopefully that triggers a conflict.");
