96d9bce3f122ccac74c4261cb7b56a8f9010b6a2
Andrew Petriccione <andrew.petriccione@metlife.com>
2019-01-02 13:48:30 -0500

    removed the stub method

--- MergeTestClass.java
+++ MergeTestClass.java
@@ -1,10 +1,11 @@
 
 public class MergeTestClass {
-
+		
 	public static void main(String[] args) {
+		System.out.println("Changed this whole line--hopefully that makes a conflict.");
 		System.out.println("Changed this whole line--hopefully that triggers a conflict.");
 		System.out.println("Changed this whole line--hopefully that triggers a conflict.");
-		System.out.println("Changed this whole line--hopefully that triggers a conflict.");
+		
 	}
 
 }
